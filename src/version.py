5943c0357671d82eb505dce1403806618970ae7c
glebulon <gleb.zverinskiy@gmail.com>
2026-02-26 16:39:05 -0500

    version 1.1.4

--- src/version.py
+++ src/version.py
@@ -10,7 +10,7 @@ GAME_NAME = "SimLaps Client"
 # Version components
 VERSION_MAJOR = 1
 VERSION_MINOR = 1
-VERSION_PATCH = 3
+VERSION_PATCH = 4
 
 # Full version string
 VERSION = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
